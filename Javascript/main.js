a6291b5bf9952b7a6a79da4e8162beb0f90507af
d3xsky <dmisic@gmail.com>
2018-05-17 01:15:40 +0200

    comitting changes
    added register div, added button for registration on login page. implemented jquery function that will handle showing and hiding these forms according to button click.

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,8 +1,20 @@
 $(document).ready(function() {
+
+    $('#register-div').hide();
+    // end of document.ready
+});
+
+function SwitchToRegForm() {
+
+    $('#login-div').hide();
+    $('#register-div').show();
+}
+
+function Login() {
+
     $("#login-btn").click(function(e) {
-        e.preventDefault();
-        var username = $("#forma_log_in").val();
         var password = $("#password").val();
+        var username = $("#forma_log_in").val();
         $.ajax({
             type: 'POST',
             url: 'api/loginController.php',
@@ -15,5 +27,6 @@ $(document).ready(function() {
             }
         });
     });
-    document.title = 'Expenses-Tracker';
-});
+    e.preventDefault();
+
+}
