f26fa464dcc0f69c6ce713bba7ffb9e160e0931e
dosadinko <dosadinko@hotmail.com>
2018-05-16 14:55:52 +0200

    napisao test za login controller

--- RestApi/loginController.php
+++ RestApi/loginController.php
@@ -1,1 +1,6 @@
 <?php
+require_once '../model/utils.php';
+
+$a = new utils();
+$a = $a->loginCheckByMail('dosadinko@hotmail.com', 'dosadinko95');
+echo $a;
--- model/utils.php
+++ model/utils.php
@@ -1,5 +1,16 @@
 <?php
+require_once 'connection.php';
+require_once 'expence.php';
+require_once 'account.php';
 
 class utils {
-
+    public function loginCheckByMail($email, $password){
+        $connection = new connection();
+        $query = "SELECT * FROM accounts WHERE email='$email' and password='$password'";
+        $result = mysqli_query($connection->getConnection(), $query);
+        if(empty($result)){
+            return 'radi';
+        }
+        return $result;
+    }
 }
