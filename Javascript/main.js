e1798a5cc2164be24d791a11b4b4205c6da31d5c
d3xsky <dmisic@gmail.com>
2018-05-17 17:13:18 +0200

    fixing sending requests and validation with jquery

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,63 +1,77 @@
 $(document).ready(function() {
-
-    $('#register-div').hide();
     $('#expenses-section').hide();
-    // end of document.ready
+    $('#register-div').hide();
+
+    $('#login-forma').submit(function() {
+        var username = $("#forma_log_in").val();
+        var password = $("#password").val();
+        if (username === '' || password === '') {
+            $('input[type="text"],input[type="password"]').css("border", "2px solid red");
+            $('input[type="text"],input[type="password"]').css("box-shadow", "0 0 3px red");
+            $('#login-div').append('Please enter valid credentials in order to log in.');
+
+        } else {
+            $.ajax({
+                type: "POST",
+                url: 'RestApi/loginController.php',
+                data: {
+                    username: username,
+                    password: password
+                },
+                success: function(data) {
+                    if (data === '"error"') {
+                        $('#login-div').append('Username or password incorrect.');
+                    } else if (data === '"success"') {
+                        $('#expenses-section').show();
+                        $('#register-div').hide();
+                        $('#login-div').hide();
+                    } else {
+                        $('#login-div').append('Unknown error, please try again.');
+                    }
+                }
+            });
+        }
+        //this is mandatory other wise your from will be submitted.
+        return false;
+    });
+    $('#registration').submit(function() {
+        var username = $("#reg_username").val();
+        var password = $("#reg_password").val();
+        var email = $("#reg_email").val();
+        if (username === '' || password === '' || email === '') {
+            $('input[type="text"],input[type="password"]').css("border", "2px solid red");
+            $('input[type="text"],input[type="password"]').css("box-shadow", "0 0 3px red");
+            $('#register-div').append('Please enter valid information in order to register.');
+
+        } else {
+            $.ajax({
+                type: "POST",
+                url: 'RestApi/registerController.php',
+                data: {
+                    username: username,
+                    password: password,
+                    email: email
+                },
+                success: function(data) {
+                    if (data === '"error"') {
+                        $('#register-div').append('Registration data is incorrect.');
+                    } else if (data === '"success"') {
+                        $('#register-div').append('Registration is successful. You will be redirected to login screen.');
+                        $('#register-div').slideUp(300).delay(10000).fadeOut(400);
+                        $('#login-div').show();
+                        $('#register-div').hide();
+                    } else {
+                        $('#register-div').append('Unknown error, please try again.');
+                    }
+                }
+            });
+        }
+        //this is mandatory other wise your from will be submitted.
+        return false;
+    });
 });
 
 function SwitchToRegForm() {
-
-    $('#login-div').hide();
+    $("#login-div").hide();
     $('#register-div').show();
-}
-
-$("#login-btn").click(function(e) {
-    e.preventDefault();
-    var password = $("#password").val();
-    var username = $("#forma_log_in").val();
-    $.ajax({
-        type: 'POST',
-        url: 'RestApi/loginController.php',
-        data: { username: username, password: password },
-        success: function(data) {
-            $('#expenses-section').show();
-            $('#login-div').hide();
-        },
-        error: function() {
-            console.log("error");
-        }
-    });
-});
-
-$("#register-btn-submit").click(function(e) {
-    e.preventDefault();
-    var email = $("#reg_email").val();
-    var username = $("#reg_username").val();
-    var password = $("#reg_password").val();
-    $.ajax({
-        type: 'POST',
-        url: 'RestApi/registerController.php',
-        data: { username: username, email: email, password: password },
-        success: function(data) {
-            console.log(data)
-            $('#register-div').hide();
-            $('#login-div').show();
-        },
-        error: function() {
-            console.log("error");
-        }
-    });
-
-})
-
-function Logout() {
-    $('#expenses-section').hide();
-    $('#login-div').show();
-    $.ajax({
-        type:'GET',
-        url:'RestApi/logoutController.php',
-        success:function () {
-            console.log('managed to logout');
-        }
-    })
 }
