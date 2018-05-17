7b8bdf627c8e36e3a2d2184a187e07276c0925b7
d3xsky <dmisic@gmail.com>
2018-05-17 17:44:30 +0200

    Added validation for registration and logout
    We are not sure whether to use logout functionality, for now it will use location.reload() in jquery to simply refresh the page and return to login screen.

--- Javascript/main.js
+++ Javascript/main.js
@@ -69,9 +69,47 @@ $(document).ready(function() {
         //this is mandatory other wise your from will be submitted.
         return false;
     });
+    $('#login-forma').submit(function() {
+        var username = $("#forma_log_in").val();
+        var password = $("#password").val();
+        if (username === '' || password === '') {
+            $('input[type="text"],input[type="password"]').css("border", "2px solid red");
+            $('input[type="text"],input[type="password"]').css("box-shadow", "0 0 3px red");
+            $('#login-div').append('Please enter valid credentials in order to log in.');
+
+        } else {
+            $.ajax({
+                type: "POST",
+                url: 'RestApi/loginController.php',
+                data: {
+                    username: username,
+                    password: password
+                },
+                success: function(data) {
+                    if (data === '"error"') {
+                        $('#login-div').append('Username or password incorrect.');
+                    } else if (data === '"success"') {
+                        $('#expenses-section').show();
+                        $('#register-div').hide();
+                        $('#login-div').hide();
+                    } else {
+                        $('#login-div').append('Unknown error, please try again.');
+                    }
+                }
+            });
+        }
+        //this is mandatory other wise your from will be submitted.
+        return false;
+    });
 });
 
 function SwitchToRegForm() {
     $("#login-div").hide();
     $('#register-div').show();
+}
+
+function Logout() {
+
+    location.reload();
+
 }
